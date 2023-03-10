aa5729d7f7936e9e0b1875da03afd0b47c35c8c9
rasvanB <not.obelix@gmail.com>
2023-03-10 18:17:27 +0200

    cleanup

--- lockfile-watcher.ts
+++ lockfile-watcher.ts
@@ -1,31 +1,24 @@
 import { exec } from "child_process";
 import * as z from "zod";
 import * as fs from "fs";
-import * as os from "os";
 
 const lockfileSchema = z.string().array().min(5);
 
 const DEFAULT_PATH = "C:/Riot Games/League of Legends/lockfile";
 
-const getProcessLocation = (processName: string) => {
+const getProcessLocation = (processName: string): Promise<string> => {
   return new Promise((resolve, reject) => {
-    const listCommand =
-      os.platform() === "win32"
-        ? `wmic process where name='${processName}' get commandline`
-        : `ps -A | grep ${processName}`;
+    const listCommand = `wmic process where name='${processName}' get commandline`;
 
     exec(listCommand, (err, stdout: string, _) => {
       if (err) reject(err);
+      let output = stdout.replace("CommandLine", "").trim();
 
-      if (stdout.includes('"')) {
-        resolve(
-          stdout
-            .split('"')[1]
-            .split(stdout.split('"')[1].includes("\\") ? "\\" : "/")
-            .slice(0, -1)
-            .join("/")
-        );
-      } else reject("Process not found");
+      if (output.includes('"')) {
+        resolve(output.split('"')[1].replace(processName, ""));
+        return;
+      }
+      reject("Process not found");
     });
   });
 };
@@ -34,7 +27,7 @@ export const getLockfileLocation = async () => {
   if (fs.existsSync(DEFAULT_PATH)) return DEFAULT_PATH;
   try {
     const location = await getProcessLocation("LeagueClientUx.exe");
-    return location + "/lockfile";
+    return location + "lockfile";
   } catch (error) {
     throw new Error(
       "Lockfile not found, please run League of Legends Client first!"
